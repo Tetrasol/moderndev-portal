b76c07965c2020fb63d3475e236b97ea7acc1730
gariveradlt <garivera89@gmail.com>
2015-05-14 21:19:06 -0500

    added repead cards & working on layout

--- app/mdo-card/mdo-card-directive.js
+++ app/mdo-card/mdo-card-directive.js
@@ -10,8 +10,8 @@
    * @description
    *
    * @example
-     <example module="mdoCard">
-       <file name="index.html">
+     <example module='mdoCard'>
+       <file name='index.html'>
         <mdo-card></mdo-card>
        </file>
      </example>
@@ -30,10 +30,28 @@
       controllerAs: 'mdoCard',
       controller: function () {
         var vm = this;
+        vm.articles = [{
+          name: 'spider-senses 101',
+          authorName: 'ParkerPeters',
+          description: 'welcome to the jungle',
+          image: 'http://images1.fanpop.com/images/image_uploads/Jak-and-Daxter-jak-and-ratchet-1189130_128_128.jpg',
+          tags: ['tech', 'potatos']
+        },
+        {
+          name: 'Upside Down Camera Shots',
+          authorName: 'ParkerPeters',
+          description: 'welcome to the jungle',
+          image: '',
+          tags: ['tech', 'potatos']
+        },
+        {
+          name: 'Its tough been super',
+          authorName: 'ParkerPeters',
+          description: 'welcome to the jungle',
+          image: '',
+          tags: ['tech', 'potatos']
+        }];
         vm.name = 'mdoCard';
-        vm.description = 'hello my name is g';
-        vm.authorName = 'gr';
-        vm.tags = ['angular', 'html'];
       },
       link: function (scope, element, attrs) {
         /*jshint unused:false */
